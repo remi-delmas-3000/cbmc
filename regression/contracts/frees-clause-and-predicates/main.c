f634766368dc1ce997db4be3ae131d416ec7be6a
Remi Delmas <delmasrd@amazon.com>
2022-08-25 00:04:55 -0400

    CONTACTS: update regression test suite for DFCC
    Updated tests
    - update assigns-enforce-malloc-zero
    - update assigns-local-composite
    - update assigns-replace-ignored-return-value
    - update assigns-replace-malloc-zero
    - update assigns-slice-targets
    - update assigns-slice-targets
    - update assigns_enforce_01
    - update assigns_enforce_02
    - update assigns_enforce_03
    - update assigns_enforce_04
    - update assigns_enforce_05
    - update assigns_enforce_06
    - update assigns_enforce_07

--- regression/contracts/frees-clause-and-predicates/main.c
+++ regression/contracts/frees-clause-and-predicates/main.c
@@ -1,9 +1,17 @@
+#include <stdbool.h>
 #include <stdlib.h>
 
 // A function defining freeable pointers
 void foo_frees(char *arr, const size_t size, const size_t new_size)
 {
   __CPROVER_freeable(arr);
+}
+
+bool is_freeable(void *arr)
+{
+  bool is_dynamic_object = (arr == 0) | __CPROVER_DYNAMIC_OBJECT(arr);
+  bool has_offset_zero = (arr == 0) | (__CPROVER_POINTER_OFFSET(arr) == 0);
+  return is_dynamic_object & has_offset_zero;
 }
 
 // Function that resizes the array
@@ -23,13 +31,30 @@ __CPROVER_ensures(
       __CPROVER_return_value == __CPROVER_old(arr))
 // clang-format on
 {
+  // The harness  allows to add a nondet offset to the pointer passed to foo.
+  // Proving this assertion shows that the __CPROVER_is_freeable(arr) assumption
+  // is in effect as expected for the verification
+  __CPROVER_assert(is_freeable(arr), "arr is freeable");
+
   if(arr && new_size > size)
   {
     free(arr);
-    return malloc(new_size);
+    arr = malloc(new_size);
+
+    // write at some nondet i (should be always allowed since arr is fresh)
+    size_t i;
+    if(i < new_size)
+      arr[i] = 0;
+
+    return arr;
   }
   else
   {
+    // write at some nondet i
+    size_t i;
+    if(i < size)
+      arr[i] = 0;
+
     return arr;
   }
 }
@@ -38,6 +63,8 @@ int main()
 {
   size_t size;
   size_t new_size;
+  __CPROVER_assume(size < __CPROVER_max_malloc_size);
+  __CPROVER_assume(new_size < __CPROVER_max_malloc_size);
   char *arr = malloc(size);
   arr = foo(arr, size, new_size);
   return 0;
