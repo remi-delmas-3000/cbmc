f9c278894082a574b171f2bd8bd3d8e41b937385
Remi Delmas <delmasrd@amazon.com>
2022-10-03 19:45:18 -0400

    fix test history-pointer-replace-02
    - do not match snapshot variable names
    - also check the post condition outside of the function

--- regression/contracts/history-pointer-replace-02/main.c
+++ regression/contracts/history-pointer-replace-02/main.c
@@ -1,17 +1,16 @@
 #include <assert.h>
 
 void foo(int *x) __CPROVER_assigns(*x) __CPROVER_requires(*x == 0)
-  __CPROVER_ensures(*x >= __CPROVER_old(*x) + 2)
+  __CPROVER_ensures(*x >= __CPROVER_old(*x))
 {
-  *x = *x + 2;
+  *x = *x + 1;
 }
 
 int main()
 {
-  int n = 0;
-  foo(&n);
-
-  assert(n >= 2);
-
+  int x = 0;
+  int old_x = x;
+  foo(&x);
+  assert(x >= old_x);
   return 0;
 }
