6ff282db0d273033d5d8f42fcaac0438c3c6dbbd
Remi Delmas <delmasrd@amazon.com>
2022-10-03 19:42:05 -0400

    improve test function-pointer-contracts-replace

--- regression/contracts/function-pointer-contracts-replace/main.c
+++ regression/contracts/function-pointer-contracts-replace/main.c
@@ -5,10 +5,10 @@
 typedef void (*arr_fun_t)(char *arr, size_t size);
 
 // A contract for the arr_fun_t type
-void contract(char *arr, size_t size)
+void arr_fun_contract(char *arr, size_t size)
   // clang-format off
 __CPROVER_requires((size > 0 && __CPROVER_is_fresh(arr, size)))
-__CPROVER_assigns(arr[0]) 
+__CPROVER_assigns(arr[0])
 __CPROVER_ensures(arr[0] == 0)
 // clang-format on
 {
@@ -16,24 +16,24 @@ __CPROVER_ensures(arr[0] == 0)
 
 arr_fun_t foo(arr_fun_t infun, arr_fun_t *outfun)
   // clang-format off
-__CPROVER_requires_contract(infun, contract)
-__CPROVER_ensures_contract(*outfun, contract)
-__CPROVER_ensures_contract(__CPROVER_return_value, contract)
+__CPROVER_requires_contract(infun, arr_fun_contract)
+__CPROVER_ensures_contract(*outfun, arr_fun_contract)
+__CPROVER_ensures_contract(__CPROVER_return_value, arr_fun_contract)
 // clang-format on
 {
-  *outfun = contract;
+  *outfun = arr_fun_contract;
   return infun;
 }
 
 void main()
 {
   // establish pre-conditions before replacement
-  arr_fun_t infun = contract;
+  arr_fun_t infun = arr_fun_contract;
 
   arr_fun_t outfun1 = NULL;
   arr_fun_t outfun2 = foo(infun, &outfun1);
 
   // checking post-conditions after replacement
-  assert(outfun1 == contract);
-  assert(outfun2 == contract);
+  assert(outfun1 == arr_fun_contract);
+  assert(outfun2 == arr_fun_contract);
 }
