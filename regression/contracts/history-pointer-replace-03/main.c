8872dc5de8c27031a0bcf5a09fc44495f7374bf5
Remi Delmas <delmasrd@amazon.com>
2022-10-03 19:46:22 -0400

    fix history-pointer-replace-03
    Test post expected post-condition in the harness

--- regression/contracts/history-pointer-replace-03/main.c
+++ regression/contracts/history-pointer-replace-03/main.c
@@ -2,17 +2,16 @@
 
 void foo(int *x) __CPROVER_assigns(*x)
   __CPROVER_requires(*x == __CPROVER_old(*x))
-    __CPROVER_ensures(*x == __CPROVER_old(*x) + 2)
+    __CPROVER_ensures(*x == __CPROVER_old(*x) + 1)
 {
-  *x = *x + 2;
+  *x = *x + 1;
 }
 
 int main()
 {
-  int n = 0;
-  foo(&n);
-
-  assert(n == 2);
-
+  int x;
+  int old_x = x;
+  foo(&x);
+  assert(x == old_x + 1);
   return 0;
 }
